5d659910531465aa574748360c97202758f29db6
GeChallengeM <52377234+GeChallengeM@users.noreply.github.com>
2021-05-22 02:22:05 +0200

    update template to actual plugin

--- src/test/java/com/zeroremover/ZeroRemoverPluginTest.java
+++ src/test/java/com/zeroremover/ZeroRemoverPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.zeroremover;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ZeroRemoverPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ZeroRemoverPlugin.class);
 		RuneLite.main(args);
 	}
 }
